dabb2937f3f8bb26e97fbd32eff3562e50c31f6d
Amr Abdelkamel <amromoorie@gmail.com>
2024-03-03 13:52:43 +0300

    fix (styles): scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				poppins: ['Poppins', 'sans-serif'],
-			},
-			colors: {
-				green: '#118F4B',
-				orange: '#D94111',
-				accent: '#53ACFF',
-			},
-		},
-	},
-	plugins: [ require('tailwind-scrollbar')],
-}
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        green: '#118F4B',
+        orange: '#D94111',
+        accent: '#53ACFF',
+      },
+      screens: {
+        xl: '1400px', // => @media (min-width: 1400px)
+      },
+    },
+  },
+  plugins: [],
+};
